5d2a9eca9229f8d49827a0f4aa032fa489fa03e3
Andrew <ajw35@pdx.edu>
2024-06-11 20:10:28 -0500

    middleware and protected routes

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,1 +1,33 @@
-export { auth as middleware } from "@/auth"
+import { auth } from "@/auth"
+import { authRoutes, publicRoutes } from "./routes";
+import { NextResponse } from "next/server";
+
+export default auth((req) => {
+    const { nextUrl } = req;
+    const isLoggedIn = !!req.auth;
+
+    const isPublic = publicRoutes.includes(nextUrl.pathname);
+    const isAuthRoute = authRoutes.includes(nextUrl.pathname);
+
+    if(isPublic) {
+        return NextResponse.next();
+    }
+
+    if(isAuthRoute) {
+        if(isLoggedIn) {
+            return NextResponse.redirect(new URL("/", nextUrl));
+        } else {
+            return NextResponse.next();
+        }
+    }
+
+    if(!isLoggedIn && !isPublic) {
+        return NextResponse.redirect(new URL("/login", nextUrl));
+    }
+
+    return NextResponse.next();
+});
+
+export const config = {
+    matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)']
+}
